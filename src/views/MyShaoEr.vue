b75e4fa7a9e189a50b04fbf067be7cb1a146209e
ARES丶\86176 <750788741@qq.com>
2022-03-28 10:07:34 +0800

    first commit

---
+++ src/views/MyShaoEr.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        少儿
+    </div>
+</template>
+<script>
+
+export default ({
+    name:"MyShaoEr"
+})
+</script>
+<style scoped>
+
+</style>
---
+++ src/views/MyYuLe.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        娱乐
+    </div>
+</template>
+<script>
+
+export default ({
+    name:"MyYule"
+})
+</script>
+<style scoped>
+
+</style>
